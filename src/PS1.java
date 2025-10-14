b8323c82d3fd71b3ab0e1f5f47f8a1664c487ac3
sharmadotexe <rockvikas300@gmail.com>
2025-10-14 19:07:03 +0530

    initial commit

---
+++ src/PS1.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.BeforeMethod;
+
+public class PS1 {
+
+    public void doThis(){
+        System.out.println("Method from parent class");
+    }
+
+    @BeforeMethod
+    public void beforeRun(){
+        System.out.println("Before method from parent class");
+    }
+}
---
+++ src/PS2.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class PS2 extends PS1{
+
+    @Test
+    public void newRun(){
+        doThis();
+    }
+}
